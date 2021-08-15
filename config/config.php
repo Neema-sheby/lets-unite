38ea082617040bd25ca925c4d12651197c83156b
Neema-sheby <73499706+Neema-sheby@users.noreply.github.com>
2021-08-15 06:18:07 +0100

    InitialCommit

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@ session_start();
 $timezone = date_default_timezone_set("Europe/London");
 
 
-$con = mysqli_connect("localhost","u253840030_neemasheby","hIMalaYA6795", "u253840030_letsuniteDB");
+$con = mysqli_connect("localhost","u253840030_neemasheby","*cNstzv7Y", "u253840030_letsuniteDB");
 
 if(mysqli_connect_errno()){
   echo "Failed to connect :".mysqli_connect_errno();
