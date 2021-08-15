64c8a7b62a78d48c81d89b0c61aeae49b11bba65
Neema-sheby <73499706+Neema-sheby@users.noreply.github.com>
2021-08-15 06:45:22 +0100

    InitialCommit

--- config/config.php
+++ config/config.php
@@ -11,12 +11,12 @@ session_start();
 $timezone = date_default_timezone_set("Europe/London");
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+$con = mysqli_connect($servername, $username, $password, $database);
 // Check connection
-if (!$conn) {
+if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
-mysqli_close($conn);
+mysqli_close($con);
 
 ?>
