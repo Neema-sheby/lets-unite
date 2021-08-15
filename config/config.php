d5f087009a717b43415474936bcb856fe4df39b6
Neema-sheby <73499706+Neema-sheby@users.noreply.github.com>
2021-08-15 06:39:50 +0100

    'InitialCommit'

--- config/config.php
+++ config/config.php
@@ -1,17 +1,22 @@
 <?php
 ob_start(); //Turns on output buffering
+$servername = "localhost";
+$database = "u253840030_letsuniteDB";
+$username = "u253840030_neemasheby";
+$password = "*cNstzv7Y";
 
 //It helps us to store the values entered in the input in a variable called session variable. This is done so that the values can be easily corrected if there are errors.
 session_start();
+
 $timezone = date_default_timezone_set("Europe/London");
 
-
-$con = mysqli_connect("localhost","u253840030_neemasheby","*cNstzv7Y", "u253840030_letsuniteDB");
-
-if(mysqli_connect_errno()){
-  echo "Failed to connect :".mysqli_connect_errno();
-}else{
-  echo"connected";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+echo "Connected successfully";
+mysqli_close($conn);
 
 ?>
