a405c41842132d6b1f5eb7f3c6cdf37c7b8d1c12
Neema-sheby <73499706+Neema-sheby@users.noreply.github.com>
2021-08-15 07:01:54 +0100

    'InitialCommit'

--- config/config.php
+++ config/config.php
@@ -16,7 +16,4 @@ $con = mysqli_connect($servername, $username, $password, $database);
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
-//mysqli_close($con);
-
 ?>
